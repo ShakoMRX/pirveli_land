a4fb55e6c09a74af8dff6bd62fa59396a9397511
bakurikokhodze <b_kokhodze2@cu.edu.ge>
2023-01-31 11:32:31 +0400

    change fav icon

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ import fetchApi from '../src/utils/fetch';
 import '../styles/globals.scss'
 import "../styles/font.scss"
 import Script from 'next/script'
+import fav from "/public/assets/img/fav.png"
 
 const Languages = [
 	{name:'GB',slug:'gb'},
@@ -55,8 +56,20 @@ function MyApp(ctx){
 	},[])
 
 	return <div>
+
 		<Head>
-			<meta name="viewport" content="width=device-width,initial-scale=1"></meta>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+			<meta key="robots" name="robots" content="noindex,follow"/>
+			<meta key="googlebot" name="googlebot" content="noindex,follow"/>
+			<link rel="icon"
+			      type="image/png"
+			      href={fav.src}/>
+
+			<link rel="apple-touch-icon" sizes="180x180" href={fav.src}/>
+			<link rel="icon" type="image/png" sizes="32x32" href={fav.src}/>
+			<link rel="icon" type="image/png" sizes="16x16" href={fav.src}/>
+			<link rel="manifest" href={fav.src}/>
+
 		</Head>
 
 		<Script id="my-script" strategy="afterInteractive" dangerouslySetInnerHTML={{
@@ -79,60 +92,67 @@ function MyApp(ctx){
 	</div>
 }
 
-MyApp.getInitialProps = async function ({ctx:{req}}){
-	// const protocol = req.headers['x-forwarded-proto'] || 'http'
-	// const baseUrl = `http://${req.headers.host}`
-	// console.log('ctx', baseUrl)
-
-
-	// const appDataResp = await fetch('http://localhost:3000' + '/api/hello');
-	// const appData = await appDataResp.json();
-
-	const navigation = [
-		{name:'მაღაზია',url:process.env.SHOP_LINK,slug:'shop'},
-		{name:'ჯანდაცვა',url:process.env.MEDICAL_LINK,slug:'medical'},
-		{name:'ვაუჩერები',url:process.env.VOUCHER_LINK,slug:'voucher'},
-		{name:'გათამაშება',url:process.env.WIN_LINK,slug:'winning'},
-		{name:'გართობა',url:process.env.GAME_LINK,slug:'game'},
-	]
-	let resp;
-
-	const headers = {
-		// 'Authorization': `Bearer ${process.env.appToken}`
+MyApp.getInitialProps = async function (
+	{
+		ctx:{
+			req
+		}
 	}
-
-	// const userResp = await fetch(`${process.env.API_URL}/racoon-transactions/user`, { headers })
-
-	// if (userResp.status == 200) {
-
-	//   const data = await userResp.text();
-	//   resp = userResp.status == 200 ? { id: data } : null;
-
-	//   const userAvatarResp = await fetch(`${process.env.API_URL}/user/user/get-auth-user-avatar`, { headers })
-	//   const userPointsResp = await fetch(`${process.env.API_URL}/racoon-transactions/vouchers/get-user-points`, {
-	// headers })
+)
+	{
+		// const protocol = req.headers['x-forwarded-proto'] || 'http'
+		// const baseUrl = `http://${req.headers.host}`
+		// console.log('ctx', baseUrl)
 
 
-	//   if (userAvatarResp.status == 200) {
-	//     const userAvatar = await userAvatarResp.json();
-	//     resp = Object.assign(resp, { avatar: userAvatar })
-	//   }
+		// const appDataResp = await fetch('http://localhost:3000' + '/api/hello');
+		// const appData = await appDataResp.json();
 
-	//   if (userPointsResp.status == 200) {
-	//     const userPoints = await userPointsResp.json();
-	//     resp = Object.assign(resp, { points: userPoints })
-	//   }
+		const navigation = [
+			{name:'მაღაზია',url:process.env.SHOP_LINK,slug:'shop'},
+			{name:'ჯანდაცვა',url:process.env.MEDICAL_LINK,slug:'medical'},
+			{name:'ვაუჩერები',url:process.env.VOUCHER_LINK,slug:'voucher'},
+			{name:'გათამაშება',url:process.env.WIN_LINK,slug:'winning'},
+			{name:'გართობა',url:process.env.GAME_LINK,slug:'game'},
+		]
+		let resp;
 
-	//   resp = Object.assign(resp, { id: data });
-	// }
+		const headers = {
+			// 'Authorization': `Bearer ${process.env.appToken}`
+		}
+
+		// const userResp = await fetch(`${process.env.API_URL}/racoon-transactions/user`, { headers })
+
+		// if (userResp.status == 200) {
+
+		//   const data = await userResp.text();
+		//   resp = userResp.status == 200 ? { id: data } : null;
+
+		//   const userAvatarResp = await fetch(`${process.env.API_URL}/user/user/get-auth-user-avatar`, { headers })
+		//   const userPointsResp = await fetch(`${process.env.API_URL}/racoon-transactions/vouchers/get-user-points`, {
+		// headers })
 
 
-	return {
-		appData:{
-			navigation
-		},
-		user:resp
+		//   if (userAvatarResp.status == 200) {
+		//     const userAvatar = await userAvatarResp.json();
+		//     resp = Object.assign(resp, { avatar: userAvatar })
+		//   }
+
+		//   if (userPointsResp.status == 200) {
+		//     const userPoints = await userPointsResp.json();
+		//     resp = Object.assign(resp, { points: userPoints })
+		//   }
+
+		//   resp = Object.assign(resp, { id: data });
+		// }
+
+
+		return {
+			appData:{
+				navigation
+			},
+			user:resp
+		}
 	}
-}
 
 export default MyApp
